b8b72d85f17f34b428df733d85aadd4179598f4a
yared <yaredberhanubodena@gmail.com>
2026-07-13 17:11:17 +0300

    feat: implement CheckoutPage component and update routing; enhance HomePage title

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes,Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<HomePage /> /}
-      <Route path="checkout" element={<div>test checkout page</div>} />
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
     
   );
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,6 +6,9 @@ export function HomePage() {
     return (
 
 <>
+
+    <title>Ecomerce Project</title>
+
     <div className="header">
       <div className="left-section">
         <a href="index.html" className="header-link">
